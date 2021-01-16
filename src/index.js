9fd6a198fcd41a38b5e2bf9fbebfeca7bb878814
Christian Santos <csantos1517@gmail.com>
2021-01-16 22:58:42 +0800

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-const displayController = (() => {
+import loadHeader from './modules/load-header';
+import loadContent from './modules/load-content';
+import loadFooter from './modules/load-footer';
+
+const render = (() => {
+  let bodyElement = document.getElementsByTagName('body')[0];
 
 
-  _initialize = (() => {
-    console.log('start initialize')
+  const _initialize = (() => {
+    loadHeader(bodyElement);
+    loadContent(bodyElement);
+    loadFooter(bodyElement);
   })();
 })();
